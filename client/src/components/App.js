f850a247e80d8f33d49e0fe1603830ebec245f9f
syedishmam <syedishmam17@gmail.com>
2019-08-22 16:48:07 -0400

    Add two routes in BrowserRouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
+const PageOne = () => {
+    return <div>PageOne</div>
+}
+
+const PageTwo = () => {
+    return <div>PageTwo</div>
+}
+
 class App extends React.Component {
     render() {
         return (
-            <div>App</div>
+            <div>
+                <BrowserRouter>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/pagetwo" component={PageTwo} />
+                </BrowserRouter>
+            </div>
         )
     }
 }
 
-export default App;
+export default App; 
