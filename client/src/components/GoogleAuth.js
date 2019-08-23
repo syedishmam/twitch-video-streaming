4154874f4016901de6fa2d5e668b8bed2a207c96
syedishmam <syedishmam17@gmail.com>
2019-08-23 16:21:59 -0400

    Initialize OAuth library

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
     componentDidMount() {
-        window.gapi.load('client:auth2');
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '569810483363-ag3m1mtv4veh4nvvnvk5gvs5aeabtv7h.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
     }
 
     render() {
