656627e2c8e2a6d51276b5617d08eee654525b6a
syedishmam <syedishmam17@gmail.com>
2019-08-23 19:57:43 -0400

    Display two input fields when renderInput called

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component {
+    renderInput(formProps) {
+        return <input />
+    }
+
     render() {
         return (
             <form>
-                <Field name="title" />
-                <Field name="description" />
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
             </form>
         )
     }
