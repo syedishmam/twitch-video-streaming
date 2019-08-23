b68b13b901e157a46cd6d1623da087bc1a467b2a
syedishmam <syedishmam17@gmail.com>
2019-08-23 16:43:02 -0400

    Create getAuthChange

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -11,8 +11,13 @@ class GoogleAuth extends React.Component {
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
                 this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
+    }
+
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSigned.get()})
     }
 
     renderAuthButton() {
