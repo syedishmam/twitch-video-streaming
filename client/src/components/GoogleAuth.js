1ce9a4a63f2d46987bef0d1824f1c8594a7e0619
syedishmam <syedishmam17@gmail.com>
2019-08-23 16:36:23 -0400

    Bug: remove extra curly brace

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -26,7 +26,7 @@ class GoogleAuth extends React.Component {
     }
 
     render() {
-        return <div>{this.renderAuthButton()}}</div>
+        return <div>{this.renderAuthButton()}</div>
     }
 }
 
