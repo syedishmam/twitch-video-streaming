b44a9803d40c4f08042a649099e7a58f225e474d
syedishmam <syedishmam17@gmail.com>
2019-08-23 16:50:03 -0400

    onClick added to sign in/out methods

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -19,20 +19,28 @@ class GoogleAuth extends React.Component {
     onAuthChange = () => {
         this.setState({isSignedIn: this.auth.isSigned.get()})
     }
+    
+    onSignIn = () => {
+        this.auth.signIn()
+    }
+
+    onSignOut = () => {
+        this.auth.signOut()
+    }
 
     renderAuthButton() {
         if(this.state.isSignedIn === null) { 
             return null
         } else if(this.state.isSignedIn) {
             return (
-                <button className="ui red google button">
+                <button className="ui red google button" onClick={this.onSignOut}>
                     <i className="google icon" />
                     Sign Out
                 </button>
             )
         } else {
             return (
-                <button className="ui red google button">
+                <button className="ui red google button" onClick={this.onSignIn}>
                     <i className="google icon" />
                     Sign In With Google
                 </button>
