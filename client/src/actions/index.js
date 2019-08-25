c0a395362c7334cf694f4500a91db2c88a4d42aa
syedishmam <syedishmam17@gmail.com>
2019-08-25 19:02:35 -0400

    Import all action types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from './types';
+import {SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} from './types';
 
 export const signIn = (userId) => {
     return  {
@@ -19,3 +19,4 @@ export const createStream = (formValues) => async (dispatch) => {
     const response = await streams.post('/streams', formValues);
     dispatch({type: CREATE_STREAM, payload: response.data});
 }
+
