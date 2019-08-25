8007c50316e3d7806a52fbae6c8add2fb869a7ce
syedishmam <syedishmam17@gmail.com>
2019-08-25 18:46:54 -0400

    Import redux thunk

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
+import reduxThunk from 'reduxThunk';
 
 import App from './components/App';
 import reducers from './reducers';
