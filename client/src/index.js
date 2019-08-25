9b389842a0e217f2414c8280caa9c8f4f1240f29
syedishmam <syedishmam17@gmail.com>
2019-08-25 18:47:21 -0400

    Pass reduxThunk to middleware

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import reducers from './reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware())
+    composeEnhancers(applyMiddleware(reduxThunk))
     );
 
 ReactDOM.render(
