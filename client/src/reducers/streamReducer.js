346098dd4317eb6761690cc3c6d507406dc0529f
syedishmam <syedishmam17@gmail.com>
2019-08-25 21:31:50 -0400

    Add cases to switch

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -2,6 +2,16 @@ import {CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} 
 
 export default (state = {}, action) => {
     switch(action.type) {
+        case FETCH_STREAM: 
+            return {...state, [action.payload.id]: action.payload};
+
+        case CREATE_STREAM: 
+            return {...state, [action.payload.id]: action.payload}
+
+        case EDIT_STREAM:
+            return {...state, [action.payload.id]: action.payload}
+        
+
         default:
             return state;
     }
