b405a0e852ac4cd3250cdd54c8516b31acd884be
syedishmam <syedishmam17@gmail.com>
2019-08-26 17:52:03 -0400

    Import typo

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
-import {CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} from '..actions/types';
+import {CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM} from '../actions/types';
 
 export default (state = {}, action) => {
     switch(action.type) {
+        case FETCH_STREAMS:
+            return {...state, ..._.mapKeys(action.payload, 'id')}
+
         case FETCH_STREAM: 
             return {...state, [action.payload.id]: action.payload};
 
