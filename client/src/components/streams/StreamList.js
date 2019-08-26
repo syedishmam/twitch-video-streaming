54d62e98da71ec947565be677a9ada0e49a25221
syedishmam <syedishmam17@gmail.com>
2019-08-26 18:01:43 -0400

    Create mapStateToProps, get streams state

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -2,8 +2,20 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStreams} from '../../actions';
 
-const StreamList = () => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+    return {streams: Object.values(state.streams) /*Takes an object and returns an array of it's values*/}
+}
+
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
