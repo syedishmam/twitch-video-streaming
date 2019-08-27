8eae67ef51c9a8651670075b9d5f9d8c59338dda
syedishmam <syedishmam17@gmail.com>
2019-08-27 17:21:54 -0400

    Import Router, import history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 class App extends React.Component {
     render() {
         return (
             <div className="ui container">
-                <BrowserRouter>
+                <Router history={history}>
                     <Header />
                     <div>
                         <Route path="/" exact component={StreamList} />
@@ -20,7 +21,7 @@ class App extends React.Component {
                         <Route path="/streams/delete" exact component={StreamDelete} />
                         <Route path="/streams/show" exact component={StreamShow} />
                     </div>
-                </BrowserRouter>
+                </Router>
             </div>
         )
     }
