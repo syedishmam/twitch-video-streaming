59dbfffdae41ecd097c807408aad6d6dc08233b8
syedishmam <syedishmam17@gmail.com>
2019-08-30 18:26:39 -0400

    Create mapStateToProps

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
 
-const StreamShow = () => {
-    return <div>StreamShow</div>
+class StreamShow extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        return (
+            <div>Stream Show</div>
+        );
+    }
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow);
