cd94269d4533bc1afadd9b274e6868174ca400a2
syedishmam <syedishmam17@gmail.com>
2019-08-30 16:41:56 -0400

    Add delete/edit buttons. Update styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,7 +5,14 @@
      return ReactDOM.createPortal(
         <div className="ui dimmer modals visible active">
             <div className="ui standard modal visible active">
-                Hello World
+                <div className="header">Delete Stream</div>
+                <div className="content">
+                    Are you sure you want to delete this stream?
+                </div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.getElementById('modal')
